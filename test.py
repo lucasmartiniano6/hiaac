42eff6f18a74ed18dff0d7ff4b4b6b4d57764901
Lucas Martiniano <l231036@dac.unicamp.br>
2023-08-27 18:57:56 -0300

    a

--- test.py
+++ test.py
@@ -105,6 +105,10 @@ def main():
     train_ds = CIFAR100(root = 'data/', download = True, transform = train_tfms)
     valid_ds = CIFAR100(root = 'data/', train = False, transform = valid_tfms)
 
+    benchmark = SplitCIFAR100(n_experiences=20, seed=42)
+    train_ds = benchmark.train_stream[0].dataset
+    valid_ds = benchmark.test_stream[0].dataset
+
     print(train_ds)
     print(valid_ds)
     print(len(train_ds))
@@ -215,7 +219,7 @@ class BasicBlock(nn.Module):
         return out
 
 
-class ResNet(nn.Module):
+class ResNet(ImageClassificationBase):
     def __init__(self, block, num_blocks, num_classes, nf):
         super(ResNet, self).__init__()
         self.in_planes = nf
@@ -252,42 +256,9 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
-
-
 def SlimResNet34(nclasses, nf=20):
     """Slimmed ResNet18."""
     return ResNet(BasicBlock, [3, 4, 6, 3], nclasses, nf)
-
-
-class ResNet9(ImageClassificationBase):
-    def __init__(self, in_channels, num_classes):
-        super().__init__()
-        # 3 x 32 x 32
-        self.conv1 = conv_block(in_channels, 64)         # 64 x 32 x 32
-        self.conv2 = conv_block(64, 128, pool=True)      # 128 x 16 x 16
-        self.res1 = nn.Sequential(conv_block(128, 128), 
-                                  conv_block(128, 128))  # 128 x 16 x 16
-        
-        self.conv3 = conv_block(128, 256, pool=True)    # 256 x 8 x 8
-        self.conv4 = conv_block(256, 512, pool=True)    # 512 x 4 x 4
-        self.res2 = nn.Sequential(conv_block(512, 512), 
-                                  conv_block(512, 512))  # 512 x 4 x 4
-        
-        self.classifier = nn.Sequential(nn.MaxPool2d(4), # 512 x 1 x 1
-                                        nn.Flatten(),     # 512
-                                        nn.Dropout(0.2),  
-                                        nn.Linear(512, num_classes)) # 100
-        
-    def forward(self, xb):
-        out1 = self.conv1(xb)
-        out2 = self.conv2(out1)
-        out3 = self.res1(out2) + out2
-        out4 = self.conv3(out3)
-        out5 = self.conv4(out4)
-        out6 = self.res2(out5) + out5
-        out = self.classifier(out6)
-        return out
-
 
 def denormalize(images, means, stds):
     means = torch.tensor(means).reshape(1, 3, 1, 1)
