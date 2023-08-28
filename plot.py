be15f7647b0f9a223bfc2b2b39a6b5f7a02a7de2
Lucas Martiniano <l231036@dac.unicamp.br>
2023-08-27 21:03:09 -0300

    ok

--- plot.py
+++ plot.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 38.60 36.10 27.60 22.95 14.92 13.03 10.49 10.02 8.60 10.62 8.02 7.68 6.94 6.31 6.39 5.36 7.65 4.74 3.80 3.80
-
 with open("res.txt", "r") as f:
-    y = [float(i) for i in f.read().split()]
+    y = []
+    for line in f.readlines():
+        curr = [float(i) for i in line.split()]
+        y.append(curr)
+    y = np.mean(y, axis=0)
+    print('mean_acc', y)
     x = np.linspace(5,100, len(y))
     plt.scatter(x,y, c='r')
     plt.plot(x, y, 'r-')
