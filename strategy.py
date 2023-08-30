b5b550fe2683d488bc83a6df656467d098632e4c
Lucas Martiniano <l231036@dac.unicamp.br>
2023-08-30 19:09:26 -0300

    a

--- strategy.py
+++ strategy.py
@@ -157,7 +157,7 @@ class Strategy:
         print(f'Average accuracy for all  experiences: {total_acc:.2f} %')
         with open("res.txt", "a") as f:
             f.write("==========================================\n")
-            f.write("CURR EXP: " + curr_exp + "\n")
+            f.write("CURR EXP: " + str(curr_exp) + "\n")
             for i in range(len(acc_list)):
                 f.write("   EXP " + str(i) + ": " + str(acc_list[i]) + "\n")
             f.write('\n')
